6552969607e3dca3bb42ac5e270f1a4cb5279ae7
Rajiya Sultana <rssumu04@gmail.com>
2024-05-30 00:37:54 +0600

    Solve an easy level problem.

--- Bhaskara's_Formula.cpp
+++ Bhaskara's_Formula.cpp
@@ -1,6 +1,25 @@
 #include<iostream>
+#include<cmath>
+#include<iomanip>
 using namespace std;
 int main (){
+    double x,y,z,m;
+    cout << "Enter 3 numers: ";
+    cin >> x >> y >> z;
+    
+    m = pow(y,2) - (4 * x * z);
+    if(m > 0 && x != 0){
+        double R1 = (-y+ sqrt(m)) / (2 * x);
+        double R2 = (-y- sqrt(m)) / (2 * x);
+
+        cout << fixed << setprecision(5);
+        cout << "R1 = " << R1 << endl;
+        cout << "R2 = " << R2 << endl;
+    }
+    else {
+        cout << "Impossivel calcular" << endl;
+    }
+
     
     return 0;
 }
